1100b2f5c0f02bb1f708d6dea7cb787464b526c1
ana-max <anastasiamaximovna17@mail.ru>
2021-03-22 15:26:55 +0500

    dataloader для таргет

--- src/middleware/targetsDataLoaderInit.ts
+++ src/middleware/targetsDataLoaderInit.ts
@@ -2,7 +2,7 @@ import {MiddlewareFn} from "type-graphql";
 import DataLoader from "dataloader";
 import {Context} from "@umk-stat/statistic-server-core";
 import {setLoaderToContext} from "@umk-stat/statistic-server-core";
-import { Target } from "@umk-stat/statistic-server-client-graphql";
+import { Target, findTargetsBySystemId } from "@umk-stat/statistic-server-client-graphql";
 
 export const targetsDataLoaderInit: MiddlewareFn<Context> = (
     { context, args },
@@ -13,7 +13,7 @@ export const targetsDataLoaderInit: MiddlewareFn<Context> = (
 
     const batchFn: (ids: string[]) => Promise<Target[][]> =  async (ids: string[])
         : Promise<Target[][]> => {
-        const targets = await Promise.all(ids.map(id => context.clientDatabaseApi.queries.findTargetsBySystemId(id)));
+        const targets = await Promise.all(ids.map(id => findTargetsBySystemId(context, id)));
         return targets;
     };
 
